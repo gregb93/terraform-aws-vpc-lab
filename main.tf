0130c81b2ace4dc267ab1c4a8fd025a6112be195
Gregory Brown <gregb93@comcast.net>
2026-09-24 13:42:31 -0400

    Add public route table and subnet associatioin

--- main.tf
+++ main.tf
@@ -48,3 +48,22 @@ resource "aws_internet_gateway" "igw" {
     Name = "terraform-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "terraform-public-rt"
+  }
+}
+
+# Assicate Public Subnete with Public Route aws_route_table
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
